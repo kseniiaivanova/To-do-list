1dd087d5dccd1a7420a863e8463e84b7b281eb04
Kseniia Ivanova <kseniia.ivanova@gmail.com>
2022-11-03 19:12:12 +0100

    Sebbe fixade allt

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,63 @@
-let todoOne = {
-  content: "Buy plane tickets",
-  done: false,
-};
+class Todo {
+  constructor(id, content, done) {
+    this.id = id;
+    this.content = content;
+    this.done = done;
+  }
+}
 
-let todoTwo = {
-  content: "Book a hotel",
-  done: false,
-};
+let todoOne = new Todo(Date.now(), "Buy plane tickets", false);
 
-let todoThree = {
-  content: "Pack my things",
-  done: false,
-};
+let todoTwo = new Todo(Date.now(), "Book a hotel", false);
+
+let todoThree = new Todo(Date.now(), "Pack my things", false);
 
 let todos = [todoOne, todoTwo, todoThree];
 
-//function displayTodos() {
-for (let i = 0; i < todos.length; i++) {
+let addButton = document.getElementById("mybtn");
+addButton.addEventListener("click", function () {
+  let inputField = document.getElementById("myinput");
+  let inputText = inputField.value;
+  let userInput = new Todo(Date.now(), inputText, false);
+
+  if (inputText !== " ") {
+    todos.push(userInput);
+
+    displayTodos();
+  }
+});
+
+function displayTodos() {
   let myList = document.getElementById("myList");
-  let check = document.createElement("input");
-  check.setAttribute("type", "checkbox");
+  myList.innerHTML = "";
 
-  let item = document.createElement("li");
-  item.classList.add("li");
+  for (let i = 0; i < todos.length; i++) {
+    let item = document.createElement("li");
+    let check = document.createElement("input");
+    let todoText = document.createElement("span");
+    check.setAttribute("type", "checkbox");
 
-  myList.appendChild(item);
-  item.appendChild(check);
-  item.innerHTML += todos[i].content;
+    item.className = "li";
+    //check.innerHTML = "Test";
 
-  check.addEventListener("change", () => {
-    console.log("hej!");
-    if (check.checked) {
-      console.log("oj");
-    } else {
-      console.log("hej");
+    if (todos[i].done) {
+      item.classList.add("checked");
     }
-  });
+
+    check.addEventListener("click", () => {
+      if (todos[i].done === false) {
+        todos[i].done = true;
+      } else {
+        todos[i].done = false;
+      }
+      displayTodos();
+    });
+
+    item.appendChild(check);
+    item.appendChild(todoText);
+    myList.appendChild(item);
+
+    todoText.innerHTML += todos[i].content;
+  }
 }
+displayTodos();
